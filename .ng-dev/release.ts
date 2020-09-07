a38d7659a6a6d7adab5f61e1e0d3d9830f216a6a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-07 22:39:34 +0200

    Update release.ts

--- .ng-dev/release.ts
+++ .ng-dev/release.ts
@@ -5,8 +5,8 @@ import {ReleaseConfig} from '../dev-infra/release/config';
 /** Configuration for the `ng-dev release` command. */
 export const release: ReleaseConfig = {
   npmPackages: [
-    '@test/animations',
-    '@test/bazel',
+    '@angular/animations',
+    '@angular/bazel',
   ],
   // TODO: Implement release package building here.
   buildPackages: async () => {
