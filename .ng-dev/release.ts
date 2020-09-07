119759ef701af6d20c62aae3d39180009199487c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-09-07 21:12:12 +0200

    Update release.ts

--- .ng-dev/release.ts
+++ .ng-dev/release.ts
@@ -5,27 +5,20 @@ import {ReleaseConfig} from '../dev-infra/release/config';
 /** Configuration for the `ng-dev release` command. */
 export const release: ReleaseConfig = {
   npmPackages: [
-    '@angular/animations',
-    '@angular/bazel',
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/compiler-cli',
-    '@angular/core',
-    '@angular/elements',
-    '@angular/forms',
-    '@angular/language-service',
-    '@angular/localize',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-    '@angular/platform-server',
-    '@angular/platform-webworker',
-    '@angular/platform-webworker-dynamic',
-    '@angular/router',
-    '@angular/service-worker',
-    '@angular/upgrade',
+    '@test/animations',
+    '@test/bazel',
   ],
   // TODO: Implement release package building here.
-  buildPackages: async () => [],
+  buildPackages: async () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          {name: "@test/animations", outputPath: join(__dirname, '../dist/animations')},
+          {name: "@test/bazel", outputPath: join(__dirname, '../dist/bazel')},
+        ]);
+      }, 5000);
+    });
+  },
   // TODO: This can be removed once there is a org-wide tool for changelog generation.
   generateReleaseNotesForHead: async () => {
     exec('yarn -s gulp changelog', {cwd: join(__dirname, '../')});
